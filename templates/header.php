85f25651892c996bf8dbd3d7c68ee5b8ab2b695e
Diogo Alberto <kirchoffalberto@gmail.com>
2023-08-10 17:49:56 -0300

    Finalizando a classe de Mensagens

--- templates/header.php
+++ templates/header.php
@@ -2,9 +2,15 @@
 
     require_once("globals.php");
     require_once("db.php");
+    require_once("models/Message.php");
 
-    $flassMessage = [];
+    $message = new Message($BASE_URL);
 
+    $flassMessage = $message->getMessage();
+
+    if (!empty($flassMessage["msg"])) {
+        $message->clearMessage();
+    }
 ?>
 
 <!DOCTYPE html>
