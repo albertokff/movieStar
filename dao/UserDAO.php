b0e50d8a1246874f1bfa7b14ed7bf6d94c8ab2e6
Diogo Alberto <kirchoffalberto@gmail.com>
2023-08-15 09:01:23 -0300

    Registro de Usuário

--- dao/UserDAO.php
+++ dao/UserDAO.php
@@ -1,15 +1,18 @@
 <?php
 
     require_once("models/User.php");
+    require_once("models/Message.php");
 
     class UserDAO implements UserDAOInterface {
 
         private $conn;
         private $url;
+        private $message;
 
         public function __construct(PDO $conn, $url) {
             $this->conn = $conn;
             $this->url = $url;
+            $this->message = new Message($url);
         }
 
         public function buildUser($data) {
@@ -31,6 +34,26 @@
 
         public function create(User $user, $authUser = false) {
 
+            $stmt = $this->conn->prepare("INSERT INTO users(
+                name, lastname, email, password, token
+                ) VALUES (
+                    :name, :lastname, :email, :password, :token
+                )");
+
+            $stmt->bindParam(":name", $user->name);
+            $stmt->bindParam(":lastname", $user->lastname);
+            $stmt->bindParam(":email", $user->email);
+            $stmt->bindParam(":password", $user->password);
+            $stmt->bindParam(":token", $user->token);
+
+            $stmt->execute();
+            
+            if ($authUser) {
+                $this->setTokenToSession($user->token);
+            }
+            
+
+
         }
 
         public function update(User $user) {
@@ -42,6 +65,12 @@
         }
 
         public function setTokenToSession($token, $redirect = true) {
+
+            $_SESSION["token"] = $token;
+
+            if ($redirect) {
+                $this->message->setMessage("Seja bem-vindo!", "success", "editprofile.php");
+            }
 
         }
 
