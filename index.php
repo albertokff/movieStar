577afefe3d0567073822ac15c76379ff5198109e
Diogo Alberto <kirchoffalberto@gmail.com>
2023-08-15 17:37:38 -0300

    Exibindo filmes na Home

--- index.php
+++ index.php
@@ -1,35 +1,34 @@
 <?php
     require_once("templates/header.php");
+    require_once("dao/MovieDAO.php");
+
+    $movieDao = new MovieDAO($conn, $BASE_URL);
+
+    $latestMovies = $movieDao->getLatestMovies();   
+
+    $actionMovies = [];
+
+    $comedyMovies = [];
+
 ?>
 
     <div id="main-container" class="container-fluid">
         <h2 class="section-title">Filmes Novos</h2>
         <p class="section-description">Veja as críticas dos últimos filmes adicionados no MovieStar</p>
         <div class="movies-container">
-            <div class="card movie-card">
-                <div class="card-img-top" style="background-image: url('<?= $BASE_URL ?>img/movies/movie_cover.jpg')"></div>
-                <div class="card-body">
-                    <p class="card-rating">
-                        <i class="fas fa-star"></i>
-                        <span class="rating">9</span>
-                    </p>
-                    <h5 class="card-title">
-                        <a href="#">Título do Filme</a>
-                    </h5>
-                    <a href="#" class="btn btn-primary rate-btn">Avaliar</a>
-                    <a href="#" class="btn btn-primary card-btn">Conhecer</a>
-                </div>
-            </div>
+            <?php foreach ($latestMovies as $movie): ?>
+                <?php require("templates/movie_card.php"); ?>
+            <?php endforeach; ?>            
         </div>
         <h2 class="section-title">Ação</h2>
         <p class="section-description">Veja os melhores filmes de ação</p>
         <div class="movies-container">
-
+            
         </div>
         <h2 class="section-title">Comédia</h2>
         <p class="section-description">Veja os melhores filmes de comédia</p>
         <div class="movies-container">
-
+            
         </div>
     </div>
 
