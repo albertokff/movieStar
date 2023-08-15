16dc94fb74d604296bd66104b9c340ce90f56ce7
Diogo Alberto <kirchoffalberto@gmail.com>
2023-08-15 09:50:51 -0300

    Alterando o header e fazendo o logout

--- dao/UserDAO.php
+++ dao/UserDAO.php
@@ -62,6 +62,23 @@
 
         public function verifyToken($protected = false) {
 
+            if(!empty($_SESSION["token"])) {
+
+                $token = $_SESSION["token"];
+
+                $user = $this->findByToken($token);
+
+                if ($user) {
+                    return $user;
+                } else if ($protected) {
+                    $this->message->setMessage("Faça a autenticação para acessar esta página!", "error", "index.php");
+                }
+
+            } else if ($protected) {
+                $this->message->setMessage("Faça a autenticação para acessar esta página!", "error", "index.php");
+            }
+
+
         }
 
         public function setTokenToSession($token, $redirect = true) {
@@ -109,7 +126,33 @@
         }
 
         public function findByToken($token) {
+            if ($token != "") {
+                $stmt = $this->conn->prepare("SELECT * FROM users WHERE token = :token");
 
+                $stmt->bindParam(":token", $token);
+
+                $stmt->execute();
+
+                if ($stmt->rowCount() > 0) {
+                    
+                    $data = $stmt->fetch();
+                    $user = $this->buildUser($data);
+
+                    return $user;
+
+                } else {
+                    return false;
+                }
+
+            } else {
+                return false;
+            }
+        }
+
+        public function destroyToken() {
+            $_SESSION["token"] = "";
+
+            $this->message->setMessage("Logout realizado com sucesso!", "success", "index.php");
         }
 
         public function changePassword(User $user) {
