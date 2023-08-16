ddf959b6cc98a6f8e369abd163bc6898decc1dc7
Diogo Alberto <kirchoffalberto@gmail.com>
2023-08-16 13:48:18 -0300

    Criando model de Review

---
+++ review_process.php
@@ -0,0 +1,20 @@
+<?php
+
+    require_once("globals.php");
+    require_once("db.php");
+    require_once("models/Movie.php");
+    require_once("models/Message.php");
+    require_once("dao/UserDAO.php");
+    require_once("dao/MovieDAO.php");
+
+    $message = new Message($BASE_URL);
+    $userDao = new UserDAO($conn, $BASE_URL);
+    $movieDao = new MovieDAO($conn, $BASE_URL);
+
+    $type = filter_input(INPUT_POST, "type");
+
+    $userData = $userDao->verifyToken();
+
+    if($type === "create") {
+
+    }
